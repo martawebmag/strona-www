01d0e7065087891e44fd743627f10988b2950b93
Marta Gałecka <galecka_marta@wp.pl>
2024-03-19 13:19:55 +0100

    added links to website

--- includes/footer.php
+++ includes/footer.php
@@ -1,9 +1,9 @@
 <footer>
         <p> &copy; <?php echo date ("Y"); ?> | Marta Gałecka WEB-MAG
             <span class="icon-box">
-                <a href="#"><i class="fa-brands fa-square-twitter"></i></a>
+                <a href="https://www.linkedin.com/in/marta-ga%C5%82ecka-8b0553294/"><i class="fa-brands fa-linkedin"></i></a>
                 <a href="#"><i class="fa-brands fa-square-facebook"></i></a>
-                <a href="#"><i class="fa-brands fa-square-github"></i></a>
+                <a href="https://github.com/martawebmag"><i class="fa-brands fa-square-github"></i></a>
             </span>
         </p>
     </footer>
